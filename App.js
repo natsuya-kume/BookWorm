b78c13a319ff763e46c12953348c47dcd2df7cee
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2020-10-15 03:10:38 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,134 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import BookCount from "./components/BookCount";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      totalCount: 0,
+      readingCount: 0,
+      readCount: 0,
+      isAddNewBookVisible: false,
+    };
+  }
+
+  showAddNewBook = () => {
+    this.setState({ isAddNewBookVisible: true });
+  };
+
+  hideAddNewBook = () => {
+    this.setState({ isAddNewBookVisible: false });
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <SafeAreaView />
+        <View
+          style={{
+            height: 70,
+            borderBottomWidth: 0.5,
+            borderBottomColor: "#E9E9E9",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ fontSize: 24 }}>Book Worm</Text>
+        </View>
+        <View style={{ flex: 1 }}>
+          {this.state.isAddNewBookVisible && (
+            <View style={{ height: 50, flexDirection: "row" }}>
+              <TextInput
+                placeholder="本のタイトルを入力してください"
+                placeholderTextColor="grey"
+                style={{ flex: 1, backgroundColor: "#ececec", paddingLeft: 5 }}
+              />
+              <TouchableOpacity>
+                <View
+                  style={{
+                    width: 50,
+                    height: 50,
+                    backgroundColor: "#a5deba",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Ionicons name="ios-checkmark" color="white" size={40} />
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={this.hideAddNewBook}>
+                <View
+                  style={{
+                    width: 50,
+                    height: 50,
+                    backgroundColor: "#deada5",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Ionicons name="ios-close" color="white" size={40} />
+                </View>
+              </TouchableOpacity>
+            </View>
+          )}
+          <TouchableOpacity
+            onPress={this.showAddNewBook}
+            style={{ position: "absolute", bottom: 20, right: 20 }}
+          >
+            <View
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 25,
+                backgroundColor: "#AAD1E6",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text style={{ color: "white", fontSize: 30 }}>+</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            height: 70,
+            borderTopWidth: 0.5,
+            borderTopColor: "#E9E9E9",
+            flexDirection: "row",
+          }}
+        >
+          <BookCount title="Total" count={this.state.totalCount} />
+          <BookCount title="Reading" count={this.state.readingCount} />
+          <BookCount title="Read" count={this.state.readCount} />
+        </View>
+        <SafeAreaView />
+      </View>
+    );
+  }
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    height: 50,
+    width: 50,
+    backgroundColor: "red",
   },
 });
