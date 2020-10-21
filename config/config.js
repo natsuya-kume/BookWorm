1195f3f3e3ccc7988e6178ee47c8d897a8513896
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2020-10-22 03:10:46 +0900

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-DHnqTBUXQcuxlosBeA6UhRp8cdnRCuA",
+  authDomain: "chat-app-e75bb.firebaseapp.com",
+  databaseURL: "https://chat-app-e75bb.firebaseio.com",
+  projectId: "chat-app-e75bb",
+  storageBucket: "chat-app-e75bb.appspot.com",
+  messagingSenderId: "785353073671",
+  appId: "1:785353073671:web:c74a1bd1a373a9680ff6a2",
+};
